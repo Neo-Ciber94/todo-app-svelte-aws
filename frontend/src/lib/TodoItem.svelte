12cc8ab44aba94e578b7ec4c26dd3afa4129622f
Freddy L <neo-ciber_94@hotmail.com>
2023-05-26 18:53:27 -0400

    minor changes

--- frontend/src/lib/TodoItem.svelte
+++ frontend/src/lib/TodoItem.svelte
@@ -6,10 +6,8 @@
   import { toast } from "@zerodevx/svelte-toast";
   import { getErrorMessage } from "@/utils/getErrorMessage";
   import toastThemes from "@/utils/toastThemes";
-  import { createEventDispatcher } from "svelte";
   import events from "@/utils/events";
-
-  const dispatch = createEventDispatcher();
+  import { fade, fly } from "svelte/transition";
 
   export let todo: TodoModel;
 
@@ -43,34 +41,40 @@
   };
 </script>
 
-<Link to={`/edit/${todo.id}`}
-  ><div class="border border-gray-400/50 rounded-md shadow py-2 px-4 bg-white">
-    <div class="text-gray-800 flex flex-row justify-between items-center">
-      <div class="flex flex-row items-center">
-        <input
-          type="checkbox"
-          class="mr-3 text-indigo-600 focus:ring-indigo-700 h-5 w-5"
-          bind:checked={todo.done}
-          on:click|stopPropagation={handleToggle}
-        />
-        <div class={`font-bold ${todo.done ? "line-through opacity-50" : ""}`}>
-          {todo.title}
+{#key todo.id}
+  <Link to={`/edit/${todo.id}`}
+    ><div
+      class="border border-gray-400/50 rounded-md shadow py-2 px-4 bg-white"
+    >
+      <div class="text-gray-800 flex flex-row justify-between items-center">
+        <div class="flex flex-row items-center">
+          <input
+            type="checkbox"
+            class="mr-3 text-indigo-600 focus:ring-indigo-700 h-5 w-5"
+            bind:checked={todo.done}
+            on:click|stopPropagation={handleToggle}
+          />
+          <div
+            class={`font-bold ${todo.done ? "line-through opacity-50" : ""}`}
+          >
+            {todo.title}
+          </div>
         </div>
+
+        <button
+          on:click|stopPropagation|preventDefault={handleDelete}
+          class="text-red-500 hover:text-red-700 text-3xl flex flex-row items-center justify-center w-4 h-4"
+        >
+          <FaRegTrashAlt />
+        </button>
       </div>
 
-      <button
-        on:click|stopPropagation|preventDefault={handleDelete}
-        class="text-red-500 hover:text-red-700 text-3xl flex flex-row items-center justify-center w-4 h-4"
-      >
-        <FaRegTrashAlt />
-      </button>
+      <hr class="border border-gray-400/20 my-1" />
+      <div class="flex flex-row justify-between">
+        <p class={`py-2 ${todo.done ? "line-through opacity-30" : ""}`}>
+          {todo.content}
+        </p>
+      </div>
     </div>
-
-    <hr class="border border-gray-400/20 my-1" />
-    <div class="flex flex-row justify-between">
-      <p class={`py-2 ${todo.done ? "line-through opacity-30" : ""}`}>
-        {todo.content}
-      </p>
-    </div>
-  </div>
-</Link>
+  </Link>
+{/key}
